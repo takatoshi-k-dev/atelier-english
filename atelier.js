8cb8aa71f2011a2d61a55fa67ac95a16cc8c2235
takatoshi-k-dev <atelierenglish.official@gmail.com>
2026-09-01 17:35:59 +0900

    Add files via upload

--- atelier.js
+++ atelier.js
@@ -560,7 +560,7 @@
   }
 
   /* ---------- 版の表示（制作中だけ・完成したら外す） ---------- */
-  const VER = 'v.0901-1';
+  const VER = 'v.0901-2';
   (function(){
     const el=document.createElement('div');
     el.id='ver'; el.textContent=VER;
